67148727ae41012ad943dfbb890718ebf30208a9
Abu Hanif Arnob <hanifcse90@gmail.com>
2022-10-24 20:10:02 +0600

    add file

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,5 +1,5 @@
 import { GoogleAuthProvider } from "firebase/auth";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { BiLogInCircle } from "react-icons/bi";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../../context/AuthProvider";
@@ -8,6 +8,41 @@ import "../../styles/login.css";
 const Login = () => {
   const provider=new GoogleAuthProvider();
   const {user,googleWithSinIn}=useContext(AuthContext);
+  const [userInfo,setUserInf]=useState({
+    email:"",
+    password:""
+  })
+  const [error,setError]=useState({
+    email:"",
+    password:"",
+  });
+
+
+  const handleOnsubmit=(event)=>{
+    event.prevetDefault();
+    
+  }
+  const handleOnChangeEmail=(e)=>{
+    
+    setUserInf({...userInfo,email:e.target.value})
+  }
+  const handleOnChangePassword=(e)=>{
+    const password=e.target.value;
+    if(password.length<6){
+      setError({...error,password:"Password Must Be length 7 "})
+       setUserInf({...userInfo, password:e.target.value})
+      // setUserInf({...userInfo, password:""})
+    }
+    else{
+      setError({...error,password:""})
+      setUserInf({...userInfo, password:e.target.value});
+     
+    }
+   
+  }
+  console.log(error.password);
+
+
   const handleGoogleSignIn=()=>{
     googleWithSinIn(provider)
     .then(result=>{
@@ -18,16 +53,25 @@ const Login = () => {
 
   }
 
-  console.log(user);
+
   return (
     <div className="login-container">
       <div className="login-title">
         Login
         <BiLogInCircle />
       </div>
-      <form className="login-form">
-        <input type="text" placeholder="Your Email" />
-        <input type="password" placeholder="password" />
+      <form onSubmit={handleOnsubmit} className="login-form">
+        <input type="text" placeholder="Your Email"
+          value={userInfo.email}
+           onChange={handleOnChangeEmail}
+           />
+        <input type="password" placeholder="password" 
+        value={userInfo.password} 
+        onChange={handleOnChangePassword}
+         />
+         {
+          error.password && <p className="error-message">{error.password}</p>
+         }
         <button>Login</button>
 
         <p>
